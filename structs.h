e1771b961de38573a905152b7f8bbe89e96e537a
rleongPd5 <rleong00@stuy.edu>
2019-10-16 17:42:06 -0400

    changed parameters of struct villager to be struct villager pointers

--- structs.h
+++ structs.h
@@ -1,6 +1,6 @@
 // struct villager {
 //   char* name; int id;
 // };
-struct villager examplevillager();
-struct villager printvillager(struct villager;);
-struct villager changevillager(struct villager; char *; int ;);
+struct villager* examplevillager();
+struct villager* printvillager(struct villager *v);
+struct villager* changevillager(struct villager *v, char *str, int i);
