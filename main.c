82dcd19907e6f0ea636c6cfce7c1a1d45881e0a0
rleongPd5 <rleong00@stuy.edu>
2019-10-16 17:41:32 -0400

    Tested all functions successfully

--- main.c
+++ main.c
@@ -13,8 +13,8 @@ int main() {
     struct villager v0;
     struct villager v1;
 
-    // struct villager *p0 = &v0;
-    // struct villager *p1 = &v1;
+    struct villager *p0 = &v0;
+    struct villager *p1 = &v1;
 
     v0.name = "Heather";
     v0.id = 130;
@@ -22,14 +22,21 @@ int main() {
     v1.name = "Gerry";
     v1.id = 90;
 
-    printf("v0|| name: %s\t| id: %d\n", v0.name, v0.id);
+    printf("\n\nOriginals:\nv0|| name: %s\t| id: %d\n", v0.name, v0.id);
     printf("v1|| name: %s\t| id: %d\n", v1.name, v1.id);
 
     printf("Testing examplevillager: \n");
     examplevillager();
 
     printf("Testing printvillager: \n");
-    printvillager(v0);
-    printvillager(v1);
+    printvillager(p0);
+    printvillager(p1);
+
+    printf("\nTesting changevillager: \n");
+    changevillager(p0, "Lily", 2149818);
+    printvillager(p0);
+    printf("\n");
+    changevillager(p1, "James", 8753821);
+    printvillager(p1);
     return 0;
  }
