829570c791d97ab1d19d4f10b6750fbd69e6c707
rleongPd5 <rleong00@stuy.edu>
2019-10-16 17:17:46 -0400

    tested printvillager and it works

--- main.c
+++ main.c
@@ -10,22 +10,26 @@ struct villager {
 
 int main() {
 
-    struct villager s0;
-    struct villager s1;
+    struct villager v0;
+    struct villager v1;
 
-    struct villager *p0 = &s0;
-    struct villager *p1 = &s1;
+    // struct villager *p0 = &v0;
+    // struct villager *p1 = &v1;
 
-    s0.name = "Heather";
-    s0.id = 130;
+    v0.name = "Heather";
+    v0.id = 130;
 
-    s1.name = "Gerry";
-    s1.id = 90;
+    v1.name = "Gerry";
+    v1.id = 90;
 
-    printf("s0|| name: %s\t| id: %d\n", s0.name, s0.id);
-    printf("s1|| name: %s\t| id: %d\n", s1.name, s1.id);
+    printf("v0|| name: %s\t| id: %d\n", v0.name, v0.id);
+    printf("v1|| name: %s\t| id: %d\n", v1.name, v1.id);
 
     printf("Testing examplevillager: \n");
     examplevillager();
+
+    printf("Testing printvillager: \n");
+    printvillager(v0);
+    printvillager(v1);
     return 0;
  }
