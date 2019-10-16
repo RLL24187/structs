c35be8f742fc236b1bcf1b2e82c71b74a972cf52
rleongPd5 <rleong00@stuy.edu>
2019-10-16 17:11:43 -0400

    Added headers, commented out prototype for struct villager

---
+++ structs.h
@@ -0,0 +1,6 @@
+// struct villager {
+//   char* name; int id;
+// };
+struct villager examplevillager();
+struct villager printvillager(struct villager;);
+struct villager changevillager(struct villager; char *; int ;);
